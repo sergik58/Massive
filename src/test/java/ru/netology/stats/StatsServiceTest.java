d89c8ef5369020bdc804e9f3c697f76fe7e33ca9
Сергей Поздов <torres.t2012@yandex.ru>
2022-08-03 10:29:09 +0300

    Initial commit

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void testAllSumMonth() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+        long actual = service.sum(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void AvgTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.average(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.maxSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.minSales(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountBelowAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.countBelowAvg(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testOfCountAboveAvg() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.countAboveAvg(sales);
+        Assertions.assertEquals(expected, actual);
+    }
+}
